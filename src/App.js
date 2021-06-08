79aa1883b1e94b65b85838fc98738a20bad362d4
Sachin Kumar <sachinsingh0400@gmail.com>
2021-06-08 20:02:15 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css'
+
+import './styles/styles.css'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+import Blogs from './pages/Blogs'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/blogs' component={Blogs} />
+            <Route component={NotFound} />
+          </Switch>
+      </Router>
+      {/* <div class="left_svg">
+      <svg viewBox="0 0 1440 320">
+        <path fill="#000" fill-opacity="1" d="M0,96L60,133.3C120,171,240,245,360,266.7C480,288,600,256,720,202.7C840,149,960,75,1080,90.7C1200,
+        107,1320,213,1380,266.7L1440,320L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,
+        320,60,320L0,320Z">
+        </path>
+      </svg>
+      </div> */}
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+function Navbar(){
+    var username = JSON.parse(localStorage.getItem('userdata'));
+
+    const handleSideBar = () =>{
+        let x = document.getElementById("sdi_nav");
+        if(x.className === "sdi_navbar"){
+            x.className += " sdi_navbar2";
+        }else{
+            x.className = "sdi_navbar";
+        }
+    };   
+
+    return(
+        <header className="sdi_navbar" id="sdi_nav">
+            <div className="sdi_bar" onClick={() => handleSideBar()}>
+                <div className="sdi_bar1"></div>
+                <div className="sdi_bar2"></div>
+                <div className="sdi_bar3"></div>
+            </div>
+            <Link to="/" className="sdi_navbar"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Ford_logo_flat.svg/129px-Ford_logo_flat.svg.png" alt="img"/></Link>
+            {username != null && <NavLink to="/logout" activeClassName="is-active" className="sdi_navbar1">Logout</NavLink> }
+            {/* {username == null && <NavLink to="/login" activeClassName="is-active" className="sdi_navbar1" >Login</NavLink> } */}
+            <NavLink to="/blogs" id="sdi_nav_last" activeClassName="is-active" className="sdi_navbar1">Blogs</NavLink>
+            <NavLink to="/about" activeClassName="is-active" className="sdi_navbar1">About</NavLink>
+            <NavLink to="/" exact activeClassName="is-active" className="sdi_navbar1">Home</NavLink>
+            
+        </header>
+    )
+}
+
+export default Navbar;
